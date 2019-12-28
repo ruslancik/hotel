3c650cf17528c0c3898b4c526357e367f8702379
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-12-28 23:45:47 +0300

    rooms

--- src/context.js
+++ src/context.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
+import items from './data'
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
     state = {
-
+        rooms: [],
+        featuredRooms : [],
+        sortedRooms: [],
+        loading: true
     };
+
+    componentDidMount(){
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true); // filtered featured rooms
+        this.setState({     // update the state
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        })
+    }
+
+    formatData (items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;           // get IDs
+            let images = item.fields.images.map(image => image.fields.file.url); // get direct Image URLs
+            let room = { ...item.fields, images, id}; // add all of them into new Object and return it
+            return room;
+        })
+        return tempItems; 
+    }
     
     render() {
         return (
             <div>
-                <RoomContext.Provider value='hello'>
+                <RoomContext.Provider value={{...this.state}}>
                     {this.props.children}
                 </RoomContext.Provider>
             </div>
